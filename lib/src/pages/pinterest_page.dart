2681a3d72e5f6f355f1851dd4622d53e76cd3909
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-03-01 22:08:27 -0300

    add pinteres page

--- lib/src/pages/pinterest_page.dart
+++ lib/src/pages/pinterest_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:provider/provider.dart';
 
 class Pinterest extends StatelessWidget {
@@ -18,6 +19,19 @@ class PinterestGrid extends StatelessWidget{
   Widget build(BuildContext context){
     return new StaggeredGridView.countBuilder(
       crossAxisCount:4,
-    )
+      itemCount: 8,
+      itemBuilder: (BuildContext context, int index)=>new Container(
+        color: Colors.green,
+        child: new Center(
+          child: CircleAvatar(
+            backgroundColor: Colors.white,
+            child:new Text('$index'),
+            ),
+        )),
+        staggeredTileBuilder: (int index)=>
+        new StaggeredTile.count(2, index.isEven?2:1),
+        mainAxisSpacing: 4.0,
+        crossAxisSpacing: 4.0,
+    );
   }
 }
