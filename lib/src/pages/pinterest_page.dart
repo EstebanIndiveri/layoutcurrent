b40b494a751e0a1834f2def44910b821da7e68c4
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-03-01 21:54:33 -0300

    first layout

---
+++ lib/src/pages/pinterest_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Pinterest extends StatelessWidget {
+  const Pinterest({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Holamundo'),),
+    );
+  }
+}
